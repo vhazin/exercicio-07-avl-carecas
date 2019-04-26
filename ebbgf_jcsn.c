18da9a0aea50d1bba22e7191c24f6c012d988647
Eduardo Gusmão Filho <42840856+EduardoBBGusmao@users.noreply.github.com>
2019-04-26 17:06:30 -0300

    Update ebbgf_jcsn.c

--- ebbgf_jcsn.c
+++ ebbgf_jcsn.c
@@ -11,9 +11,10 @@ typedef struct Tree {
 	int altura;
 } AvlTree;
 
-int insertnode(AvlTree* arv);
+int insertnode(AvlTree* arv, int nvfolha);
 int findInorder(AvlTree* node, int x);
 int printInorder(AvlTree* node, int x);
+
 void rotDireita(AvlTree* r);
 void rotDDireita(AvlTree* r);
 void rotDEsquerda(AvlTree* r);
@@ -42,7 +43,7 @@ int main() {
 				arvore -> altura = 0;
 			
 			} else{
-			inserirAVL(arvore,nvfolha);	
+			insertnode(arvore,nvfolha);	
 			}	
 		}
 		if (input2 == 2){
@@ -61,52 +62,52 @@ int main() {
 
 }
 
-int insertnode(AvlTree* arv){
-	int nvfolha;
-	scanf("%d", &nvfolha);	
+int insertnode(AvlTree* arv, int nvfolha){
+
 	
 	AvlTree* atual = (AvlTree*)malloc(sizeof(AvlTree));
 	atual -> val = nvfolha;
 	atual -> direita = NULL;
 	atual -> esquerda = NULL;
 	atual -> altura = 0;
-	while(1){
-		if(arv -> val < nvfolha){
-			arv -> altura = arv ->altura +1;
-			if(arv -> direita == NULL){
-				atual -> head = arv;						
-				arv ->direita = atual;
-				break;
+	printf("agora é %d", arv -> val);
+	printf(" agora é %d\n", arv -> altura);
+	
+	
+	if(arv -> val < nvfolha){
+		arv -> altura = arv ->altura +1;
+		if(arv -> direita == NULL){
+			atual -> head = arv;						
+			arv ->direita = atual;
+			
+			if(arv -> head ->altura == 2){
+				if(arv -> altura == -1)
+					rotDEsquerda(arv);	
+				if(arv -> altura == 1)
+					rotDireita(arv);	
 			}
-			arv = arv -> direita;
+			return 1;	
 		}
-		else{
+		insertnode(arv -> direita, nvfolha);
+	} else {
 			arv -> altura = arv ->altura -1;
 			if(arv -> esquerda == NULL){
-				atual -> head = arv;								
+				atual -> head = arv;			
 				arv ->esquerda = atual;
-				break;
+			if(arv -> head ->altura == -2){
+				if(arv -> altura == -1)
+					rotEsquerda(arv);	
+			
+				if(arv -> altura == 1)
+					rotDDireita(arv);	
+			
+				}
+				return 1;
 			}
-			arv = arv -> esquerda;
+			insertnode(arv -> esquerda, nvfolha);
 		}	
-		if(arv -> head ->altura == 2){
-			if(arv -> altura == -1)
-				rotDEsquerda(arv);	
 			
-			if(arv -> altura == 1)
-				rotDireita(arv);	
-			
-		}	
-		if(arv -> head ->altura == -2){
-			if(arv -> altura == -1)
-				rotEsquerda(arv);	
-			
-			if(arv -> altura == 1)
-				rotDDireita(arv);	
-			
-		}
-			
-	}
+	
 		
 }
 int findInorder(AvlTree* node, int x) { 
@@ -170,61 +171,4 @@ void rotDEsquerda(AvlTree* r){
 	noA->esquerda = r;
 	noA->head = (r)->head;
 	rotDireita(noA);
-}
-void inserirAVL (AvlTree* r, int x){
-	if(r  == NULL){
-		r -> val = x;
-		r -> esquerda = NULL;
-		r -> direita = NULL;
-		r -> altura =1 ;
-	} else {
-		if (x < r -> val){
-			inserirAVL(r->esquerda, x);
-		}	
-		if ((altura(r->esquerda)) - altura(r->direita) == 2){
-			if (x < r->esquerda->val){
-				rotDireita(r);
-			} 
-			else {
-				rotDDireita(r);			
-			}
-		}
-		else {
-			inserirAVL(r, x);
-			if(altura(r -> direita)-altura(r -> esquerda) == 2){
-				if(x > r -> direita -> val){
-					rotDireita(r);
-				}
-				else {
-					rotDEsquerda(r);
-				}
-			}
-		}
-	atualizaAltura(r);
-	}
-}
-int atualizaAltura(AvlTree *no){
-    int altDireita;
-    int altEsquerda;
-
-    if(no != NULL){
-        altDireita <- altura(no->direita);
-        altEsquerda<- altura(no->esquerda);
-        no->altura <- MAXIMO(altDireita , altEsquerda) +1;
-
-    }
-}
-
-int altura(AvlTree *no){
-    if(no == NULL)
-        return 0;
-    else
-        return no->altura;
-}
-
-int MAXIMO(int altDireita,int altEsquerda){
-    if(altDireita>altEsquerda)
-        return altDireita;
-    else
-        return altEsquerda;
 }
